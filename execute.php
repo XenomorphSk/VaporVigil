b2ef1bd0a6650a4211ceb064bfed492c70f73d1b
Kyr1o5 <69166293+XenomorphSk@users.noreply.github.com>
2024-04-24 21:55:55 -0300

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,32 +1,58 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $button = $_POST['button'];
+    $action = $_POST['action'] ?? '';
 
-    switch ($button) {
-        case 'nmap':
-            $script = './nmap.pl';
-            break;
-        case 'direnum':
-            $script = 'perl direnum.pl';
-            break;
-        case 'subenum':
-            $script = 'perl subenum.pl';
-            break;
-        case 'rid':
-            $script = 'perl rid.pl';
-            break;
-        case 'sql':
-            $script = 'perl sql.pl';
-            break;
-        default:
-            echo 'Invalid button';
-            exit;
+    if ($action === 'start') {
+        startPhpServer();
+        echo 'Servidor PHP iniciado.';
+    } elseif ($action === 'stop') {
+        stopPhpServer();
+        echo 'Servidor PHP encerrado.';
+    } else {
+        $button = $_POST['button'];
+        $target = $_POST['target'];
+
+        error_log("Button clicked: " . $button);
+        error_log("Target: " . $target);
+
+        switch ($button) {
+            case 'nmap':
+                $command = 'nmap -Pn ' . escapeshellarg($target);
+                break;
+            case 'direnum':
+                $command = 'perl direnum.pl';
+                break;
+            case 'subenum':
+                $command = 'perl subenum.pl';
+                break;
+            case 'rid':
+                $command = 'enum4linux/./enum4linux.pl ' . escapeshellarg($target);
+                break;
+            case 'sql':
+                $command = 'perl sql.pl';
+                break;
+            default:
+                echo 'Invalid button';
+                exit;
+        }
+
+        error_log("Executing command: " . $command);
+
+        // Executa o comando
+        $output = shell_exec($command);
+
+        error_log("Command output: " . $output);
+
+        // Retorna o output
+        echo $output;
     }
+}
 
-    // Executa o script Perl
-    $output = shell_exec($script);
+function startPhpServer() {
+    exec('nohup php -S localhost:8000 > /dev/null 2>&1 &');
+}
 
-    // Retorna o nome do arquivo de saída
-    echo 'output.txt';
+function stopPhpServer() {
+    exec('pkill -f "php -S localhost:8000"');
 }
 ?>
